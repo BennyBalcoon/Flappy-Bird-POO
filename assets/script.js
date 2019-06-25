81593f972eff59021034429abaf2d06dea192a43
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-06-25 17:16:30 +0200

    load img on js

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+let cvs = document.getElementById('canvas');
+let ctx = cvs.getContext('2d');
+
+let bird = new Image();
+bird.src = "assets/bird.png";
+
+let bg = new Image();
+bg.src = "assets/bg.png";
+
+let pipeNorth = new Image();
+pipeNorth.src = "assets/pipeNorth.png";
+
+let pipeSouth = new Image();
+pipeSouth.src = "assets/pipeSouth.png";
