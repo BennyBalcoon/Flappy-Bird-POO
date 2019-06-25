d9f6af85f60e07860088bd23b338baed35d01edb
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-06-25 17:33:57 +0200

    add two classes

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,29 @@ pipeNorth.src = "assets/pipeNorth.png";
 
 let pipeSouth = new Image();
 pipeSouth.src = "assets/pipeSouth.png";
+
+
+class Bird {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    moveUp(){
+        this.y -= 20;
+    }
+}
+
+const flappy = new Bird(20, 20);
+document.addEventListener("keydown", function() {
+    flappy.moveUp();
+})
+
+class Obstacle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const pipeTop = new Obstacle();
+const pipeDown = new Obstacle();
